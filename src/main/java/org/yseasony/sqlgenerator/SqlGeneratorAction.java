488c79a2dcca0b357aa3e87d5f9d040de916d853
yelei.yl <yelei.yl@alibaba-inc.com>
2017-12-27 13:37:00 +0800

    v2.1.2

--- src/main/java/org/yseasony/sqlgenerator/SqlGeneratorAction.java
+++ src/main/java/org/yseasony/sqlgenerator/SqlGeneratorAction.java
@@ -1,10 +1,11 @@
 package org.yseasony.sqlgenerator;
 
 import com.intellij.database.psi.DbTable;
-import com.intellij.database.view.DatabaseView;
 import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.yseasony.sqlgenerator.children.DeleteSqlGeneratorAction;
@@ -25,20 +26,12 @@ public class SqlGeneratorAction extends ActionGroup {
 
     @Override
     public void update(AnActionEvent e) {
-        DatabaseView view = DatabaseView.DATABASE_VIEW_KEY.getData(e.getDataContext());
-        if (view == null) {
-            e.getPresentation().setEnabledAndVisible(false);
+        PsiElement psiElement = e.getData(LangDataKeys.PSI_ELEMENT);
+        if (psiElement == null) {
             return;
         }
-        Object[] tables = view.getTreeBuilder().getSelectedElements().toArray();
-        boolean hasTable = false;
-        for (Object table : tables) {
-            if (table instanceof DbTable) {
-                hasTable = true;
-                break;
-            }
-        }
-        e.getPresentation().setEnabledAndVisible(hasTable);
+
+        e.getPresentation().setEnabledAndVisible(psiElement instanceof DbTable);
         super.update(e);
     }
 
